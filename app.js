0a095aa49618f73c338154e0061548827c7c33c6
Thu Thao <thao101191@donga.edu.vn>
2024-11-08 08:28:51 +0700

    cap nhat

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const processDatabase = async () => {
 };
 
 // Lắng nghe cổng
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server đang chạy trên cổng ${PORT}`);
 });
